a9fbb87670d03f57f98bcf94b70b7de6227f085c
Chono N <49493993+Gyarbij@users.noreply.github.com>
2024-12-09 21:32:00 +0100

    Update main.py
    Add env variable for allowed host

--- src/main.py
+++ src/main.py
@@ -8,16 +8,18 @@ from api_analytics.fastapi import Analytics
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 from routers import download, dynamic, index
 
-
 load_dotenv()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(Analytics, api_key=os.getenv('API_ANALYTICS_KEY'))
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=["gitingest.com", "*.gitingest.com", "gitdigest.dev", "localhost"])
-templates = Jinja2Templates(directory="templates")
 
-    
+# Load allowed hosts from environment variable
+allowed_hosts_env = os.getenv("ALLOWED_HOSTS", "localhost")
+allowed_hosts = [host.strip() for host in allowed_hosts_env.split(",")]
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=allowed_hosts)
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/api", response_class=HTMLResponse)
 async def api_docs(request: Request):
@@ -28,7 +30,6 @@ async def api_docs(request: Request):
 @app.get('/favicon.ico')
 async def favicon():
     return FileResponse('static/favicon.ico')
-
 
 app.include_router(index)
 app.include_router(download)
